6905b22a6db8bfc7b265415b548037a35a442897
Jonas Lucena <jonaslucena@outlook.com>
2020-05-12 20:12:40 -0300

    first commit
    sample of variables in Java

---
+++ src/logica/Variaveis.java
@@ -0,0 +1,30 @@
+package logica;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		int idade;
+		idade = 33;
+		
+		int idade1 = 33;
+		double nota = 8.6;
+		boolean ativo = true;
+		String nome = "Jonas";
+		
+		System.out.println(nota);
+		
+		System.out.println(nome + " -> " + nota);
+		
+		// int 2x
+		int x2;
+		
+		int numeroDePessoas = 10;
+		
+		var x = 5;
+		var y = "abc";
+		var z = 2.2;
+		var w = true;
+		
+	}
+}
